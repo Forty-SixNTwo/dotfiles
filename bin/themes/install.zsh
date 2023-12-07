6cce9c45ba38b8582d281d8bba4990e19dbd164a
Forty-SixNTwo <84645149+Forty-SixNTwo@users.noreply.github.com>
2023-12-07 12:29:04 +0200

    added dracula themes v2

--- bin/themes/install.zsh
+++ bin/themes/install.zsh
@@ -9,7 +9,6 @@
 ##	::: ##:::: ##:::: ##: ########: ##:::: ##: ########:. ######::
 ##	:::..:::::..:::::..::........::..:::::..::........:::......:::
 ##  ##############################################################
-
 THEMES="$HOME/.themes"
 OH_MY_ZSH="$HOME/.oh-my-zsh"
 
@@ -20,37 +19,47 @@ while true; do
 
   case $choice in
   	y)
-			echo "Starting themes installation."
-			if [[ -d $THEMES ]]; then
-				echo "Themes directory already exists."
-				if [[ -d "$THEMES/lib" ]]; then
-					echo "Themes lib directory already exists."
-				else
-					echo "Creating themes lib directory."
-					mkdir -p $THEMES/lib
-				fi
+		echo "Starting themes installation."
+		if [[ -d $THEMES ]]; then
+			echo "Themes directory already exists."
+			if [[ -d "$THEMES/lib" ]]; then
+				echo "Themes lib directory already exists."
 			else
-				echo "Creating themes directory."
-				mkdir -p $THEMES
+				echo "Creating themes lib directory."
+				mkdir -p $THEMES/lib
 			fi
+		else
+			echo "Creating themes directory."
+			mkdir -p $THEMES
+		fi
 
-			# Iterm
-			curl -sS https://raw.githubusercontent.com/dracula/iterm/master/Dracula.itermcolors -o $THEMES/Dracula.itermcolors
-			# Terminal
-			curl -sS https://raw.githubusercontent.com/dracula/terminal-app/master/Dracula.terminal -o $THEMES/Dracula.terminal
-			# ZSH
-			curl -sS https://raw.githubusercontent.com/dracula/zsh/master/dracula.zsh-theme -o $THEMES/dracula.zsh-theme
-			curl -sS https://raw.githubusercontent.com/dracula/zsh/master/lib/async.zsh -o $THEMES/lib/async.zsh
-			ln -s $THEMES/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
+		# Iterm
+		curl -sS https://raw.githubusercontent.com/dracula/iterm/master/Dracula.itermcolors -o $THEMES/Dracula.itermcolors
+		
+		# Terminal
+		curl -sS https://raw.githubusercontent.com/dracula/terminal-app/master/Dracula.terminal -o $THEMES/Dracula.terminal
+		
+		# ZSH
+		curl -sS https://raw.githubusercontent.com/dracula/zsh/master/dracula.zsh-theme -o $THEMES/dracula.zsh-theme
+		curl -sS https://raw.githubusercontent.com/dracula/zsh-syntax-highlighting/master/zsh-syntax-highlighting.sh -o $THEMES/zsh-syntax-highlighting.sh
+		curl -sS https://raw.githubusercontent.com/dracula/zsh/master/lib/async.zsh -o $THEMES/lib/async.zsh
+		ln -s $THEMES/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
+		
+		# Alfred
+		curl -sS https://raw.githubusercontent.com/dracula/alfred/master/Dracula.alfredappearance -o $THEMES/Dracula.alfredappearance
 
-			break
-			;;
-		n)
-			echo "Skipping themes installation."
-			exit 0
-			;;
+		# Midnight Commander
+		git clone https://github.com/dracula/midnight-commander.git $THEMES/midnight-commander
+		ln -s $THEMES/midnight-commander/skins/dracula.ini $HOME/.local/share/mc/skins
+		ln -s $THEMES/midnight-commander/skins/dracula256.ini $HOME/.local/share/mc/skins
+		break
+		;;
+	n)
+		echo "Skipping themes installation."
+		exit 0
+		;;
     *)
-			echo "Invalid input. Please enter 'y' or 'n'."
-			;;
+		echo "Invalid input. Please enter 'y' or 'n'."
+		;;
   esac
 done
